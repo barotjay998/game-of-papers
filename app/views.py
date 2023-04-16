c17d5af4fe7d4d657c14b1bf77a7328f2840e6aa
Jay Barot <barotjay998@yahoo.com>
2023-04-15 22:02:27 -0500

    asdf

--- app/views.py
+++ app/views.py
@@ -7,9 +7,13 @@ views_bp = Blueprint('views_bp', __name__, static_folder='static', template_fold
 def index():
     return render_template('index.html')
 
+@views_bp.route('/papers')
+def papers():
+    return render_template('papers.html')
+
 @views_bp.route('/conference')
-def conference_papers():
-    return render_template('conf-papers.html')
+def conference():
+    return render_template('conference.html')
 
 @views_bp.route('/login')
 def login():
